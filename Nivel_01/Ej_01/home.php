bbca7e04fb3e46d5ac97238173c160d3137e27d9
davidgzmina <davidmobile28@gmail.com>
2025-10-06 13:12:14 +0200

    Nivel_1 Ej_01 created

---
+++ Nivel_01/Ej_01/home.php
@@ -0,0 +1,33 @@
+<?php
+ session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home page</title>
+    <style>
+        body{ 
+            background-color: blue;
+            color: aliceblue;
+        }
+    </style>
+</head>
+<body>
+    <h1>This is the home page</h1>
+ 
+</body>
+</html>
+<?php
+
+
+    echo "Your username: " . $_SESSION["username"] ."<br>";
+    echo "Your password: " . $_SESSION["password"] ."<br>";
+    /*
+    if(isset($_POST["logout"])){
+        session_destroy();
+        header("Location : index.php");
+    }*/
+?>
---
+++ Nivel_01/Ej_01/index.php
@@ -0,0 +1,37 @@
+<?php
+ session_start();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <form action="index.php" method="post">
+
+        <h2>Username: </h2>
+        <input type="text" name="username" ><br>
+        <h2>Password:</h2> 
+        <input type="text" name="password"><br>
+        <input type="submit" name="login" value="login">
+    </form>
+</body>
+</html>
+<?php
+if(isset($_POST["login"])){
+
+    if(!empty($_POST["username"]) && !empty($_POST["password"])) {
+         
+        $_SESSION["username"] = $_POST["username"];
+        $_SESSION["password"] = $_POST["password"];
+
+        header("Location: home.php");
+    }
+    else{
+        echo"Username or password missing out";
+    }
+}
