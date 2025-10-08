6a0dbc48d5f28bfa72bad6bcc3ecfb1e02cb4292
davidgzmina <davidmobile28@gmail.com>
2025-10-08 11:03:49 +0200

    Nivel_1 Ej_02 created

---
+++ Nivel_01/Ej_02/class/refrescos.php
@@ -0,0 +1,47 @@
+<?php
+
+
+
+
+class Refrescos
+{
+    private array $Properties;
+
+    public function __set($atribute, $value)
+    {
+        if(!property_exists($this,$atribute))
+        {
+            $this-> Properties[$atribute] = $value;
+        }
+
+    }
+
+
+    public function __get($atribute)
+    {
+        return $this -> Properties[$atribute];
+    }
+
+    public function getProperties()
+    {
+        return $this -> Properties;
+    }
+
+
+}
+
+
+
+class Zumos extends Refrescos
+{
+
+}
+
+
+
+$zumos = new Zumos;
+
+$zumos -> zumos_name = "Granini";
+$zumos -> zumos_price = 3.25;
+$zumos -> zumos_stock = 250;
+
---
+++ Nivel_01/Ej_02/index.php
@@ -0,0 +1,8 @@
+<?php
+
+require_once 'class/refrescos.php';
+
+
+echo $zumos -> zumos_name . " (Marca del zumo)" . PHP_EOL;
+echo $zumos -> zumos_price . " (Precio del zumo)" . PHP_EOL;
+echo $zumos -> zumos_stock . " (Stock en almacen del zumo)" . PHP_EOL;
